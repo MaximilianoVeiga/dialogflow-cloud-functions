96b7d425c69ad158e7a4d0b53a5a78963b2f9d14
Maximiliano Neto <maxsantosneto@gmail.com>
2021-04-05 13:27:15 -0300

    Change suggestions to work with dialogflow webchat

--- functions/utils.js
+++ functions/utils.js
@@ -2,21 +2,49 @@ const { Suggestion, Card, Text, Image } = require('dialogflow-fulfillment-helper
 
 function setSuggestion(agent, title, suggestions) {
     const platform = agent.source;
-
+  
     if (platform != null) {
-        const currentSuggestions = new Suggestion({
-            title: title,
-            reply: suggestions[0],
-            platform: platform
-        });
-
-        for (let index = 1; index < suggestions.length; index++) {
-            currentSuggestions.addReply_(suggestions[index]);
-        }
-
-        agent.add(currentSuggestions);
+      const currentSuggestions = new Suggestion({
+        title: title,
+        reply: suggestions[0],
+        platform: platform,
+      });
+  
+      for (let index = 1; index < suggestions.length; index++) {
+        currentSuggestions.addReply_(suggestions[index]);
+      }
+  
+      agent.add(currentSuggestions);
+    } else {
+      let chipsMap = [];
+      
+      for (let index = 0; index < suggestions.length; index++) {
+        chipsMap[index] = {
+          "text": suggestions[index]
+        };
+      }
+  
+      const response = {
+        "richContent": [
+          [
+            {
+              "type": "chips",
+              "options": chipsMap
+            }
+          ]
+        ]
+      };
+  
+      agent.add(title);
+  
+      agent.add(
+        new Payload(agent.UNSPECIFIED, response, {
+          rawPayload: true,
+          sendAsMessage: true,
+        })
+      );
     }
-}
+  }
 
 function setCard(agent, title, text, buttonText, buttonURL) {
     const platform = agent.source;
@@ -48,6 +76,37 @@ function setResponse(agent, responses) {
     })
 }
 
+function setLink(agent, link, text) {
+    const platform = agent.source;
+  
+    if (platform != null) {
+      agent.add(text);
+    } else {
+      const response = {
+        "richContent": [
+          [
+            {
+              "link": link,
+              "text": text,
+              "icon": {
+                "type": "link",
+                "color": "#FFFFFF"
+              },
+              "type": "button"
+            }
+          ]
+        ]
+      };
+  
+      agent.add(
+        new Payload(agent.UNSPECIFIED, response, {
+          rawPayload: true,
+          sendAsMessage: true,
+        })
+      );
+    }
+  }
+
 function setImage(agent, imageUrl) {
     const platform = agent.source;
 
@@ -61,4 +120,4 @@ function findArray(response, title) {
     return response.find(element => element[`${title}`])[`${title}`];
 }
 
-module.exports = { setSuggestion, setCard, setResponse, setImage, findArray };
+module.exports = { setSuggestion, setCard, setResponse, setImage, setLink, findArray };
